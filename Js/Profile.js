bf1ab6552378668713d8c6f1c85bfd4153f4e73d
FuadJTM <56724698+FuadJTM@users.noreply.github.com>
2021-11-30 16:14:57 +0700

    Update Profile.js

--- Js/Profile.js
+++ Js/Profile.js
@@ -1,7 +1,5 @@
-/*
-THIS CODE BY LWJerri#3290
-*/
-const boxOwners = document.getElementById("owners");
+
+const Owners = document.getElementById("owners");
 const API = "https://gabut-bgt.glitch.me/user/";
 const owners = [
     {
@@ -24,6 +22,6 @@ for (let indexOne = 0; indexOne < owners.length; indexOne++) {
             }
 
             const ownerList = "<div id='trigger' class='users' style='margin: 15px;'><div class='banner'><img src='" + output.url + "'></div></br></br></br></br><h2 class='name'>" + output.username + "</h2><div class='title'><h1 id='trigger2' style='font-size: 26px; color: #000000;'>" + elementOwners.post + "</h2></div><div class='actions'><div class='follow-btn' style='margin-bottom: 5px;'><a href='" + elementOwners.GHURL + "' target='_blank'><button style='color: #000000;'>GitHub</button></a></div><div class='follow-btn'><a href='" + elementOwners.DCURL + "' target='_blank'><button style='color: #000000;'>Discord</button></a></div></div></div>"
-            boxOwners.innerHTML += ownerList;
+            Owners.innerHTML += ownerList;
         });
 } 
